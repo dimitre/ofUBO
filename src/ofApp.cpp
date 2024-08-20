ce998fc333da6d654e3f592f1a5f5845b36fd0b2
Dimitre <dimitre@users.noreply.github.com>
2024-08-20 14:26:26 -0300

    up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,13 +11,16 @@ void ofApp::draw(){
 	if (!buffer.isAllocated()) {
 		buffer.allocate(sizeof(cor), GL_DYNAMIC_DRAW);
 	}
+	buffer.bind(GL_UNIFORM_BUFFER);
 	buffer.setData(sizeof(cor), &cor, GL_DYNAMIC_DRAW);
+	buffer.unbind(GL_UNIFORM_BUFFER);
 
 	shader.begin();
 	shader.setUniform4f("cor3", cor);
 	
 	auto id = buffer.getId();
 	shader.bindUniformBlock(id, "vars");
+
 	ofDrawRectangle(100, 100, 600, 600);
 	shader.end();
 }
